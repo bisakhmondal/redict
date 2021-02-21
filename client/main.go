965046cc8b94ead8c2886fa882f0855a02b6980d
Bisakh Mondal <bisakhmondal00@gmail.com>
2021-02-21 13:38:45 +0530

    [client] handling commands from os.Args

--- client/main.go
+++ client/main.go
@@ -48,56 +48,101 @@ func main(){
 }
 
 func handleConnection(conn net.Conn){
+
+	if len(os.Args) > 1 {
+		parseArgs(conn)
+	}
+	
 	stdreader := bufio.NewReader(os.Stdin)
-	buffer := make([]byte, BUFFERSIZE)
 
 	for {
-
 		fmt.Printf("127.0.0.1:%s>> ", PORT)
 		cmd, _  := stdreader.ReadString('\n')
 		cmd = strings.Trim(cmd, "\n")
 		cmdArr := strings.Split(cmd, " ")
 
-		switch strings.ToLower(cmdArr[0]){
-
-		case "get":
-			if len(cmdArr) <2 {
-				fmt.Println("Get requires a key to fetch value")
-				continue
-			}
-			conn.Write([]byte(cmd))
-			n, _ := conn.Read(buffer)
-			fmt.Println(string(buffer[:n]))
-
-		case "put":
-			if len(cmdArr) <3 {
-				fmt.Println("Put requires a key & Value pair store")
-				continue
-			}
-
-			conn.Write([]byte(cmd))
-		case "upgrade":
-			if len(cmdArr) <2 {
-				fmt.Println("Get requires a key to fetch value")
-				continue
-			}
-			conn.Write([]byte(cmd))
-			n, _ := conn.Read(buffer)
-			fmt.Println(string(buffer[:n]))
-		case "stats":
-			conn.Write([]byte(cmd))
-			n, _ := conn.Read(buffer)
-			fmt.Println(string(buffer[:n]))
-
-		case "downgrade":
-			if len(cmdArr) <2 {
-				fmt.Println("Downgrade requires a UID of the specific user")
-				continue
-			}
-			conn.Write([]byte(cmd))
-			n, _ := conn.Read(buffer)
-			fmt.Println(string(buffer[:n]))
+		if Communicate(conn, cmdArr, cmd) {
+			return
 		}
 
 	}
+}
+
+func parseArgs(conn net.Conn){
+	i := 1
+	for i< len(os.Args) {
+		switch os.Args[i] {
+		case "get":
+			cmd := os.Args[i] +" "+ os.Args[i+1]
+			Communicate(conn, os.Args[i:i+2], cmd)
+			i+=2
+		case "put":
+			cmd := strings.Join(os.Args[i:i+3]," ")
+			Communicate(conn, os.Args[i:i+3], cmd)
+			i+=3
+		default:
+			i++
+			fmt.Println("unknown initializer: ", os.Args[i])
+		}
+	}
+}
+
+func Communicate(conn net.Conn, cmdArr []string, cmd string) bool {
+	buffer := make([]byte, BUFFERSIZE)
+	switch strings.ToLower(cmdArr[0]) {
+
+	case "get":
+		if len(cmdArr) < 2 {
+			fmt.Println("Get requires a key to fetch value")
+			return false
+		}
+		conn.Write([]byte(cmd))
+		n, _ := conn.Read(buffer)
+		fmt.Println(string(buffer[:n]))
+
+	case "put":
+		if len(cmdArr) < 3 {
+			fmt.Println("Put requires a key & Value pair store")
+			return false
+		}
+
+		conn.Write([]byte(cmd))
+
+	case "upgrade":
+		if len(cmdArr) < 2 {
+			fmt.Println("Get requires a key to fetch value")
+			return false
+		}
+		conn.Write([]byte(cmd))
+		n, _ := conn.Read(buffer)
+		fmt.Println(string(buffer[:n]))
+
+	case "stats":
+		conn.Write([]byte(cmd))
+		n, _ := conn.Read(buffer)
+		fmt.Println(string(buffer[:n]))
+
+	case "downgrade":
+		if len(cmdArr) < 2 {
+			fmt.Println("Downgrade requires a UID of the specific user")
+			return false
+		}
+		conn.Write([]byte(cmd))
+		n, _ := conn.Read(buffer)
+		fmt.Println(string(buffer[:n]))
+
+	case "close":
+		conn.Write([]byte("close"))
+		return true
+
+	default:
+		fmt.Println("Unknown Command!! Try again")
+		fmt.Println("Here is the List...")
+		fmt.Println("1. get <key>")
+		fmt.Println("2. put <key> <value>")
+		fmt.Println("3. upgrade <secret>")
+		fmt.Println("4. downgrade <uid>")
+		fmt.Println("5. close\n")
+	}
+	return false
 }
